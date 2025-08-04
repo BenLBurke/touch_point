04f2a09e3e33fc87ce801345027914b8274f8afd
BenBurke <40806608+BenLBurke@users.noreply.github.com>
2025-08-03 23:41:35 -0400

    Update rfid_test.py
    Adding while loop

--- micro_python/rfid_test.py
+++ micro_python/rfid_test.py
@@ -3,11 +3,12 @@ import RPi.GPIO as GPIO
 
 reader = SimpleMFRC522()
 
-print("Place your tag near the reader...")
+while True:
+  print("Place your tag near the reader...")
 
-try:
-    id, text = reader.read()
-    print("ID: %s\nText: %s" % (id, text))
-finally:
-    print("Cleaning up GPIO")
-    GPIO.cleanup()
+  try:
+      id, text = reader.read()
+      print("ID: %s\nText: %s" % (id, text))
+  finally:
+      print("Cleaning up GPIO")
+      GPIO.cleanup()
