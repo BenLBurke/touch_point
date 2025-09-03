d8f0cd0445813b80cbc8f046ea5a63afb3c331ec
straka <40806608+BenLBurke@users.noreply.github.com>
2025-09-02 21:11:16 -0400

    adding yoda and the force back in.

--- touch_point.py
+++ touch_point.py
@@ -49,7 +49,7 @@ all_sounds = [{'name':   'progress', 'song':     load_sound("sound_files/progres
               , {'name': 'pirates', 'song':      load_sound("sound_files/pirates.wav"), 'length': 4}
               , {'name': 'happily', 'song':      load_sound("sound_files/happily.wav"), 'length': 7}
               , {'name': 'mickey', 'song':       load_sound("sound_files/m_i_c_k_e_y.wav"), 'length': 4}
-              # , {'name': 'force', 'song':      load_sound("sound_files/force.wav"), 'length': 9}
+              , {'name': 'force', 'song':        load_sound("sound_files/yoda_force.wav"), 'length': 9}
               , {'name': 'stop_us', 'song':      load_sound("sound_files/stop_us_now.wav"), 'length': 7}
              ]
 
